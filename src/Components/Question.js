0cd57b779c386a54ac847cfd77870053ba1b74a6
MFAC <ninafroes@gmail.com>
2019-05-24 19:24:12 +0200

    feat: Add proptypes

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Question = ({content}) => (
+const Question = ({ content }) => (
   <div>
     <p>{content}</p>
   </div>
 );
 
 export default Question;
+
+Question.propTypes = {
+  content: PropTypes.string.isRequired
+};
--- src/Components/QuestionCount.js
+++ src/Components/QuestionCount.js
@@ -1,7 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const QuestionCount = ({ counter, total }) => (
-  <div>Question {counter} of {total}</div>
+  <div>
+    Question {counter} of {total}
+  </div>
 );
 
 export default QuestionCount;
+
+QuestionCount.propTypes = {
+  counter: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired
+};
