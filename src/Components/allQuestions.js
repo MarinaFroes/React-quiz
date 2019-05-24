b32738ee7741cd01a10f66e274e28284cbb4415c
MFAC <ninafroes@gmail.com>
2019-05-24 19:44:45 +0200

    feat: Add allQuestions components

---
+++ src/Components/allQuestions.js
@@ -0,0 +1,103 @@
+export const allQuestions = [
+  {
+    questionId: 1,
+    question: "What is the first name of Harry Potter's father?",
+    answerOptions: {
+      a: "James",
+      b: "Lucios",
+      c: "Sirius"
+    },
+    correctAnswer: "a: James"
+  },
+  {
+    questionId: 2,
+    question: "What is the first name of Harry Potter's mother?",
+    answerOptions: {
+      a: "Minerva",
+      b: "Lilian",
+      c: "Sibila"
+    },
+    correctAnswer: "b: Lilian"
+  },
+  {
+    questionId: 3,
+    question: "Harry Potter is from which Hogwarts' house?",
+    answerOptions: {
+      a: "Slytherin",
+      b: "Gryffindor",
+      c: "Ravenclaw"
+    },
+    correctAnswer: "b: Gryffindor"
+  },
+  {
+    questionId: 4,
+    question: "Who are Harry's best friends?",
+    answerOptions: {
+      a: "Draco and Rowena",
+      b: "Neville and Gina",
+      c: "Rony and Hermione"
+    },
+    correctAnswer: "c: Rony and Hermione"
+  },
+  {
+    questionId: 5,
+    question: "Which spell causes an object to fly to the caster?",
+    answerOptions: {
+      a: "Aguamenti",
+      b: "Alohomora",
+      c: "Accio"
+    },
+    correctAnswer: "c: Accio"
+  },
+  {
+    questionId: 6,
+    question: "Which is not one of the Unforgivable Curses?",
+    answerOptions: {
+      a: "Avada Kedavra",
+      b: "Appare Vestigium",
+      c: "Cruciatus"
+    },
+    correctAnswer: "b: Appare Vestigium"
+  },
+  {
+    questionId: 7,
+    question: "Where is the entrance to the Diagon Alley?",
+    answerOptions: {
+      a: "behind the Leaky Cauldron",
+      b: "in Hogsmeade Village",
+      c: "Privet Drive street"
+    },
+    correctAnswer: "a: behind the Leaky Cauldron"
+  },
+  {
+    questionId: 8,
+    question: "What is the middle name of Lord Voldemort?",
+    answerOptions: {
+      a: "Marvolo",
+      b: "Salazar",
+      c: "Lucius"
+    },
+    correctAnswer: "a: Marvolo"
+  },
+  {
+    questionId: 9,
+    question:
+      "Which creature has the body of a horse and head, wings, and forelegs of an eagle ?",
+    answerOptions: {
+      a: "Goblins",
+      b: "Hippogriffs",
+      c: "Centaurs"
+    },
+    correctAnswer: "b: Hippogriffs"
+  },
+  {
+    questionId: 10,
+    question: "Which is Harry's wand?",
+    answerOptions: {
+      a: "Holly and phoenix feather",
+      b: "The Elder Wand",
+      c: "Vine and dragon heartstring"
+    },
+    correctAnswer: "a: Holly and phoenix feather"
+  }
+];
