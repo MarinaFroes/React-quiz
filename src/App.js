907f610a305c8585c92d28b784616ba0ad9de869
MFAC <ninafroes@gmail.com>
2019-06-16 09:25:33 +0200

    refactor: Turn function into class

--- src/App.js
+++ src/App.js
@@ -5,33 +5,61 @@ import QuestionCount from "./Components/QuestionCount";
 import AnswerOption from "./Components/AnswerOption";
 import { allQuestions } from "./Components/allQuestions";
 
-function App() {
-  return (
-    <div className="App">
-      {allQuestions.map(question => (
-        <div key={question.questionId}>
-          <QuestionCount
-            counter={question.questionId}
-            total={allQuestions.length}
-          />
-          <Question content={question.question} />
+class App extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: "",
+      answerOptions: [],
+      answer: "",
+      answersCount: {
+        correct: 0,
+        incorrect: 0
+      },
+      result: ""
+    };
+  }
 
-          <AnswerOption
-            answer={question.answerOptions["a"]}
-            correctAnswer={question.correctAnswer}
-          />
-          <AnswerOption
-            answer={question.answerOptions["b"]}
-            correctAnswer={question.correctAnswer}
-          />
-          <AnswerOption
-            answer={question.answerOptions["c"]}
-            correctAnswer={question.correctAnswer}
-          />
-        </div>
-      ))}
-    </div>
-  );
+  componentWillMount() {
+    this.setState({
+      question: allQuestions[0].question,
+      answerOptions: allQuestions[0].answerOptions
+    });
+    console.log(this.state.question);
+    console.log(this.state.answerOptions);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {allQuestions.map(question => (
+          <div key={question.questionId}>
+            <QuestionCount
+              counter={question.questionId}
+              total={allQuestions.length}
+            />
+            <Question content={question.question} />
+
+            <AnswerOption
+              answer={question.answerOptions["a"]}
+              correctAnswer={question.correctAnswer}
+            />
+            <AnswerOption
+              answer={question.answerOptions["b"]}
+              correctAnswer={question.correctAnswer}
+            />
+            <AnswerOption
+              answer={question.answerOptions["c"]}
+              correctAnswer={question.correctAnswer}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
