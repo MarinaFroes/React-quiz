b374a24dacf2da04c342a89443eced6e73383174
MFAC <ninafroes@gmail.com>
2019-07-16 16:25:02 +0200

    feat: Add checkAnswer props

--- src/Components/AnswerOption.js
+++ src/Components/AnswerOption.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const AnswerOption = ({ answer, correctAnswer }) => (
+const AnswerOption = ({ answer, correctAnswer, checkAnswer }) => (
   <li className="answerOption">
     <input
       type="radio"
@@ -11,13 +11,17 @@ const AnswerOption = ({ answer, correctAnswer }) => (
       value={answer}
       onChange={e => {
         if (e.target.value === correctAnswer) {
+          checkAnswer(true);
           console.log(true);
         } else {
           console.log(false);
+          checkAnswer(false);
         }
       }}
     />
-    <label className="radioCustomLabel" htmlFor={answer}>
+    <label
+      className="radioCustomLabel"
+      htmlFor={answer}>
       {answer}
     </label>
   </li>
@@ -27,5 +31,6 @@ export default AnswerOption;
 
 AnswerOption.propTypes = {
   answer: PropTypes.string.isRequired,
-  correctAnswer: PropTypes.string.isRequired
+  correctAnswer: PropTypes.string.isRequired,
+  checkAnswer: PropTypes.func.isRequired
 };
