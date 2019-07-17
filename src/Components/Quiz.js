be87b219043d1473ffd4c00534e4c3c0bbc0b261
MFAC <ninafroes@gmail.com>
2019-07-17 16:27:40 +0200

    feat: Add submit button

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -3,29 +3,29 @@ import Question from "./Question";
 import AnswerOption from "./AnswerOption";
 // import PropTypes from "prop-types";
 
-const Quiz = ({ getQuestion, checkAnswer }) => (
-  <div>
+const Quiz = ({ getQuestion, checkAnswer, onSubmit }) => (
+  <form>
     <Question content={getQuestion["question"]} />
     <AnswerOption
       answer={getQuestion.answerOptions["a"]}
       correctAnswer={getQuestion.correctAnswer}
       checkAnswer={checkAnswer}
+      checked={true}
     />
     <AnswerOption
       answer={getQuestion.answerOptions["b"]}
       correctAnswer={getQuestion.correctAnswer}
       checkAnswer={checkAnswer}
+      checked={false}
     />
     <AnswerOption
       answer={getQuestion.answerOptions["c"]}
       correctAnswer={getQuestion.correctAnswer}
       checkAnswer={checkAnswer}
+      checked={false}
     />
-{/*       
-    <button onClick={() => this.setState((prevState, props) => ({
-      questionId: prevState.questionId + 1
-    }))}>Next</button> */}
-      </div>
+    <button type="submit" onClick={onSubmit}>Next</button>
+  </form>
 );
 
 export default Quiz;
